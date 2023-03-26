c7bd7c3b17a060937093ae9b1e9003c2c8a3bee3
Rmlyy <hello@rmly.dev>
2023-03-26 18:21:51 +0300

    format, try executing after if, execute command outside check

--- bot.js
+++ bot.js
@@ -8,67 +8,71 @@ const commandsAliases = {}
 
 const membersMap = new Map()
 
-fs.readdirSync('./commands').forEach(command => {
-    const cmd = require(`./commands/${command}`)
+fs.readdirSync('./commands').forEach((command) => {
+  const cmd = require(`./commands/${command}`)
 
-    commands.push(cmd.name)
+  commands.push(cmd.name)
 
-    if (cmd.aliases) {
-        cmd.aliases.forEach(alias => {
-            commandsAliases[alias] = cmd.name
-        })
-    }
+  if (cmd.aliases) {
+    cmd.aliases.forEach((alias) => {
+      commandsAliases[alias] = cmd.name
+    })
+  }
 })
 console.log(`Loaded ${commands.length} commands`)
 console.log(`Loaded ${Object.keys(commandsAliases).length} command aliases`)
 
 bot.on('ready', () => {
-    console.log('Ready')
+  console.log('Ready')
 })
 
-bot.on('error', error => {
-    console.error(error)
+bot.on('error', (error) => {
+  console.error(error)
 })
 
-bot.on('messageCreate', message => {
-    if (message.author.bot) return
-    if (message.channel.type === 1) return
-    if (!message.content.startsWith(config.prefix)) return
+bot.on('messageCreate', (message) => {
+  if (message.author.bot) return
+  if (message.channel.type === 1) return
+  if (!message.content.startsWith(config.prefix)) return
 
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g)
-    const command = args.shift().toLowerCase()
-    let cmd
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g)
+  const command = args.shift().toLowerCase()
+  let cmd
 
-    if (commands.includes(command)) cmd = require(`./commands/${command}`)
-    else if (commandsAliases[command]) {
-        cmd = require(`./commands/${commandsAliases[command]}`)
-    } else return
+  if (commands.includes(command)) cmd = require(`./commands/${command}`)
+  else if (commandsAliases[command]) {
+    cmd = require(`./commands/${commandsAliases[command]}`)
+  } else return
 
+  if (cmd) {
     try {
-        if (cmd) {
-            const perms = message.channel.permissionsOf(message.author.id)
-            let argsMsg = `You did not provide the required arguments.`
+      const perms = message.channel.permissionsOf(message.author.id)
+      let argsMsg = `You did not provide the required arguments.`
 
-            if (cmd.args && args.length != cmd.args) {
-                if (cmd.usage) argsMsg += `\nUsage: \`${cmd.usage}\``
-                return message.channel.createMessage(argsMsg)
-            }
-            if (cmd.permission && !perms.has(cmd.permission))
-                return message.channel.createMessage('You do not have permission.')
-            if (membersMap.get(message.author.id) === cmd.name)
-                return message.channel.createMessage('Please wait before executing this command again.')
+      if (cmd.args && args.length != cmd.args) {
+        if (cmd.usage) argsMsg += `\nUsage: \`${cmd.usage}\``
+        return message.channel.createMessage(argsMsg)
+      }
 
-            if (cmd.cooldown && !perms.has('administrator')) {
-                membersMap.set(message.author.id, cmd.name)
-                setTimeout(() => { membersMap.delete(message.author.id) }, cmd.cooldown * 1000)
-            }
+      if (cmd.permission && !perms.has(cmd.permission))
+        return message.channel.createMessage('You do not have permission.')
 
-            cmd.execute(message, args)
-        }
+      if (membersMap.get(message.author.id) === cmd.name)
+        return message.channel.createMessage('Please wait before executing this command again.')
+
+      if (cmd.cooldown && !perms.has('administrator')) {
+        membersMap.set(message.author.id, cmd.name)
+        setTimeout(() => {
+          membersMap.delete(message.author.id)
+        }, cmd.cooldown * 1000)
+      }
+
+      cmd.execute(message, args)
     } catch (e) {
-        console.error(e)
-        message.channel.createMessage('There was an error trying to execute this command.')
+      console.error(e)
+      message.channel.createMessage('There was an error trying to execute this command.')
     }
+  }
 })
 
 bot.connect()
--- commands/test.js
+++ commands/test.js
@@ -1,13 +1,13 @@
 module.exports = {
-    name: 'test',
-    description: 'Just a test command',
-    aliases: ['testing', 's'],
-    permission: 'administrator',
-    usage: '[arg 1], [arg 2]',
-    args: 2,
-    cooldown: 5,
+  name: 'test',
+  description: 'Just a test command',
+  aliases: ['testing', 's'],
+  permission: 'administrator',
+  usage: '[arg 1], [arg 2]',
+  args: 2,
+  cooldown: 5,
 
-    execute (message, args) {
-        message.channel.createMessage(`First argument: ${args[0]}\nSecond argument: ${args[1]}`)
-    }
+  execute(message, args) {
+    message.channel.createMessage(`First argument: ${args[0]}\nSecond argument: ${args[1]}`)
+  },
 }
